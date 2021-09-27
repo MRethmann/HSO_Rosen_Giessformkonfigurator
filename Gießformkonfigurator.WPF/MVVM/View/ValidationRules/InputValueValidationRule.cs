bcc694f6054e2afcbb4232e30abb207ec8010f2f
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-09-27 10:45:09 +0200

    quick change to structure

--- Gießformkonfigurator.WPF/MVVM/View/ValidationRules/InputValueValidationRule.cs
+++ Gießformkonfigurator.WPF/MVVM/View/ValidationRules/InputValueValidationRule.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
-namespace Gießformkonfigurator.WPF.MVVM.ViewModel.ValidationRules
+namespace Gießformkonfigurator.WPF.MVVM.View.ValidationRules
 {
     class InputValueValidationRule : ValidationRule
     {
