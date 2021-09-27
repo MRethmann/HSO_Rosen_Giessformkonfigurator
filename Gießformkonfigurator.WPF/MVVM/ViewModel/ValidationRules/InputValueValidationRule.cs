34621c0f0e6e340bf4dea8454e41f61691d29e08
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-09-27 10:32:15 +0200

    baseplate view style und validation

--- Gießformkonfigurator.WPF/MVVM/ViewModel/ValidationRules/InputValueValidationRule.cs
+++ Gießformkonfigurator.WPF/MVVM/ViewModel/ValidationRules/InputValueValidationRule.cs
@@ -8,26 +8,27 @@ using System.Windows.Controls;
 
 namespace Gießformkonfigurator.WPF.MVVM.ViewModel.ValidationRules
 {
-    class DatatypeDecimalValidationRule : ValidationRule
+    class InputValueValidationRule : ValidationRule
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             ValidationResult result = new ValidationResult(true, null);
-            int inputValue;
-            if (string.IsNullOrEmpty(value.ToString()))
+            int inputIntValue;
+            double inputDoubleValue;
+
+            if (int.TryParse(value.ToString(), out inputIntValue))
             {
-                result = new ValidationResult(false, "Das Feld darf nicht leer sein!");
-            }
-            else if (int.TryParse(value.ToString(), out inputValue))
-            {
-                if (inputValue < 0 || inputValue > 100000)
+                if (inputIntValue < 0 || inputIntValue > 100000)
                 {
                     result = new ValidationResult(false, "Der Eingabewert darf nicht negativ oder größer 100.000 sein.");
                 }
             }
-            else if (decimal.TryParse(value.ToString(), out decimal output) == false)
+            else if(double.TryParse(value.ToString(), out inputDoubleValue))
             {
-                result = new ValidationResult(false, "Eingabewert muss eine Zahl zwischen 0 und 100.000 sein.");
+                if (inputDoubleValue < 0 || inputDoubleValue > 100000)
+                {
+                    result = new ValidationResult(false, "Der Eingabewert darf nicht negativ oder größer 100.000 sein.");
+                }
             }
 
             return result;
---
+++ Gießformkonfigurator.WPF/MVVM/ViewModel/ValidationRules/MandatoryFieldValidationRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gießformkonfigurator.WPF.MVVM.ViewModel.ValidationRules
+{
+    class MandatoryFieldValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            ValidationResult result = new ValidationResult(true, null);
+            int inputValue;
+
+            /*if (string.IsNullOrEmpty(value.ToString()))
+            {
+                result = new ValidationResult(false, "Das Feld darf nicht leer sein!");
+            }*/
+
+            if (value.ToString() == "Albert")
+            {
+                result = new ValidationResult(false, "Der Eingabewert darf nicht Albert sein.");
+            }
+
+            return result;
+        }
+    }
+}
