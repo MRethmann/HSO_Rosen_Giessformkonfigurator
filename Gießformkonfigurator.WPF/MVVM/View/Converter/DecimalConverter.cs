caad7e65a74aea48b46cc5ebee5f52f96be2e70c
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-10-01 21:02:06 +0200

    added inner rings to row details. Tested stuff to avoid binding errors --> not finished

---
+++ Gießformkonfigurator.WPF/MVVM/View/Converter/DecimalConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Gießformkonfigurator.WPF.MVVM.View.Converter
+{
+    public class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal data;
+            Decimal.TryParse(value?.ToString(), out data);
+
+            return data;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal data;
+            Decimal.TryParse(value?.ToString(), out data);
+
+            return data;
+        }
+    }
+}
