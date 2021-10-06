263c5f6d9293e1a8053eef392c8b9ef758634e8d
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-10-06 23:58:37 +0200

    changed login window

--- Gießformkonfigurator.WPF/StartWindow.xaml.cs
+++ Gießformkonfigurator.WPF/StartWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Windows;
 
 namespace Gießformkonfigurator.WPF
@@ -13,6 +15,17 @@ namespace Gießformkonfigurator.WPF
         public StartWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["GießformDB"].ToString();
+            string dataSource = new SqlConnectionStringBuilder(connectionString).DataSource;
+            string initialCatalog = new SqlConnectionStringBuilder(connectionString).InitialCatalog;
+            string userId = new SqlConnectionStringBuilder(connectionString).UserID;
+            string password = new SqlConnectionStringBuilder(connectionString).Password;
+
+            Server_TextBox.Text = dataSource;
+            DB_TextBox.Text = initialCatalog;
+            User_TextBox.Text = userId;
+            Password_TextBox.Password = password;
         }
 
         private void btnOpenModal_Click(object sender, RoutedEventArgs e)
@@ -21,5 +34,12 @@ namespace Gießformkonfigurator.WPF
             mainWindow.startWindow = this;
             mainWindow.Show();
         }
+
+        private void ConnectionDataChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            //ConfigurationManager.ConnectionStrings["GießformDB"].ConnectionString = $"data source = {Server_TextBox.Text}; initial catalog = {DB_TextBox.Text}; persist security info = True; user id = {User_TextBox.Text}; password = {Password_TextBox.Password}; multipleactiveresultsets = True; application name = EntityFramework";
+            //ConfigurationManager.RefreshSection("connectionStrings");
+        }
+
     }
 }
